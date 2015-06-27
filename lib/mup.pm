e4f399cf0bb7f514f3d7b09ef798529104a97e1c
attila <attila@stalphonsos.com>
2015-06-27 11:48:39 -0500

    Sort of starting to work now; basic tests pass, still much to do.

--- lib/mup.pm
+++ lib/mup.pm
@@ -11,12 +11,18 @@ mup - perl interface to mu
   use mup;
 
   my $mu = mup->new();
-  my $results = $mu->search({ 'subject' => 'something'});
-  print $results->count." results for subject:something\n";
+
+  my @results = $mu->find({ subject => 'something'});
+  print scalar(@results)." results for subject:something\n";
 
 =head1 DESCRIPTION
 
 This is a perl interface to mu, the Maildir search-and-destroy system.
+It presents the same API as described in the L<mu-server(1)> man page.
+In fact it works by communicating with a C<mu server> process, just
+like the C<mu4e> emacs interface to mu does.
+
+=head1 METHODS
 
 =cut
 
@@ -29,18 +35,21 @@ use vars qw($VERSION);
 use Data::SExpression;
 use IPC::Open2;
 use IO::Select;
+use Time::HiRes;
 
 $VERSION = '0.1.0';
 
 sub _init {
     my $self = shift(@_);
     $self->{'opts'} ||= (@_ && ref($_[0]))? $_[0]: { @_ };
+    my $bin = $self->_opt('mu_bin','mu');
+    my $cmd = $self->_opt('mu_server_cmd','server');
     my($mu_out,$mu_in);
     $self->{'_dead'} = 0;
-    $self->{'pid'} = open2($mu_out,$mu_in,'mu','server');
+    $self->{'pid'} = open2($mu_out,$mu_in,$bin,$cmd);
     $self->{'out'} = $mu_out;
     $self->{'in'} = $mu_in;
-    $self->{'tout'} = $self->_opt('timeout',1);
+    $self->{'tout'} = $self->_opt('timeout',0.5);
     $self->{'select'} = IO::Select->new();
     $self->{'select'}->add($mu_out);
     $self->{'inbuf'} = '';
@@ -92,15 +101,17 @@ sub _read {
     my($self) = @_;
     my $restart_needed = 0;
     my @ready = $self->{'select'}->can_read($self->{'tout'});
+    my $bufsiz = $self->_opt('bufsiz',2048);
     while (@ready && !$restart_needed) {
         foreach my $handle (@ready) {
             my $buf = '';
-            my $nread = $handle->sysread($buf,1024);
+            my $nread = $handle->sysread($buf,$bufsiz);
             if (!$nread) {
                 warn("mup: mu server died - restarting") if $self->verbose();
                 $restart_needed = 1;
             } else {
                 $self->{'inbuf'} .= $buf;
+                warn("mup: <<< |$buf|\n") if $self->verbose;
             }
         }
         @ready = $self->{'select'}->can_read($self->{'tout'})
@@ -112,51 +123,48 @@ sub _read {
     return $result;
 }
 
-sub _start_block {
-}
-
-sub _end_block {
-}
-
 sub _reset_parser {
-}
-
-sub _in_block {
-}
-
-sub _append_block {
 }
 
 sub _parse1 {
     my($self) = @_;
     my $raw = $self->{'inbuf'};
     return undef unless $raw;
-    my($xcount,$left) = ($1,$2) if $raw =~ /^\376([\da-f]+)\377(.*)$/;
+    my($xcount,$left) = ($1,$2) if $raw =~ /^\376([\da-f]+)\377(.*)$/s;
     my $count = hex($xcount);
-    my $sexp = substr($left,0,$count);
+    my $nleft = length($left);
+    warn("mup: count=$count length=$nleft: |$left|\n")
+        if $self->verbose;
+    chomp(my $sexp = substr($left,0,$count));
     $self->{'inbuf'} = substr($left,$count);
     my @results = ();
     $self->_reset_parser();
     my $data = $self->{'ds'}->read($sexp);
+    warn("mup: parsed sexp: $data\n") if $self->verbose;
     my $result = $data;
     if (ref($data) eq 'ARRAY') {
         $result = {};
+        warn("mup: parsing array") if $self->verbose;
         while (scalar(@$data)) {
             my($key,$val) = splice(@$data,0,2);
+            warn("mup: key=$key val=|$val|\n") if $self->verbose;
+            $key =~ s/^://;
             $result->{$key} = $val;
         }
     }
     return $result;
 }
 
-sub _parse {
-    my($self) = @_;
-    my @results = ();
-    while (defined(my $thing = $self->_parse1())) {
-        push(@results,$thing);
-    }
-    return @results;
-}
+sub _parse { return shift->_parse1(); }
+
+=pod
+
+=head2 new
+
+Construct a new interface object; this will cause a C<mu server>
+process to be started.
+
+=cut
 
 sub new {
     my $proto = shift(@_);
@@ -167,38 +175,52 @@ sub new {
     return $proto;
 }
 
+
+=pod
+
+=head2 finish
+
+Shut down the mu server.
+
+=cut
+
 sub finish {
     my($self) = @_;
     if ($self->{'pid'}) {
         $self->{'dying'} = 1;
-        $self->{'in'}->write("\n"); # will cause EOF eventually
+        $self->{'in'}->write("cmd:quit\n"); # will cause EOF eventually
         $self->{'in'}->flush();
-        $self->_read();
+        my $junk = $self->_read();
+        warn("mup: trailing garbage in finish: |$junk|\n") if $self->verbose;
     }
-}
-
-sub DESTROY {
 }
 
 sub _refify {
     my $href = $_[0] if @_ && ref($_[0]) eq 'HASH';
     $href ||= { @_ };
+    $href;
 }
 
 sub _quote {
     my($val) = @_;
+    $val = qq|"$val"| if (!ref($val) && $val =~ /\s/);
+    $val;
 }
 
 sub _argify {
-    my $href = _argify(@_);
+    my $href = _refify(@_);
     join(' ', map { "$_:"._quote($href->{$_}) } keys(%$href));
 }
 
 sub _execute {
     my($self,$cmd,@args) = @_;
-    my $args = _argify($cmd,@args);
+    my $args = _argify(@args);
     my $cmdstr = "cmd:$cmd $args";
-    warn("mup: <<< $cmdstr\n") if $self->verbose;
+    warn("mup: >>> $cmdstr\n") if $self->verbose;
+    if ($self->{'inbuf'}) {
+        my $junk = $self->{'inbuf'};
+        warn("mup: pitching |$junk|\n") if $self->verbose;
+    }
     $self->{'inbuf'} = '';
     $self->{'in'}->write("$cmdstr\n");
     $self->{'in'}->flush();
@@ -216,23 +238,119 @@ sub _execute {
 #    return $self->_execute($cmd,@_)
 #}
 
+=pod
+
+=head2 add
+
+Add a document to the database.
+
+=cut
+
 sub add { shift->_execute('add',@_); }
+
+
+
+=pod
+
+=head2 contacts
+
+Search contacts.
+
+=cut
 
 sub contacts { shift->_execute('contacts',@_); }
 
+
+
+=pod
+
+=head2 extract
+
+=cut
+
 sub extract { shift->_execute('extract',@_); }
+
+
+
+=pod
+
+=head2 find
+
+Blah.
+
+=cut
 
 sub find { shift->_execute('find',@_); }
 
+
+
+=pod
+
+=head2 index
+
+Blah.
+
+=cut
+
 sub index { shift->_execute('index',@_); }
+
+
+
+=pod
+
+=head2 move
+
+Blah.
+
+=cut
 
 sub move { shift->_execute('move',@_); }
 
+
+
+=pod
+
+=head2 ping
+
+Blah.
+
+=cut
+
 sub ping { shift->_execute('ping',@_); }
+
+
+
+=pod
+
+=head2 mkdir
+
+Blah.
+
+=cut
 
 sub mkdir { shift->_execute('mkdir',@_); }
 
+
+
+=pod
+
+=head2 remove
+
+Blah.
+
+=cut
+
 sub remove { shift->_execute('remove',@_); }
+
+
+
+=pod
+
+=head2 view
+
+Blah.
+
+=cut
 
 sub view { shift->_execute('view',@_); }
 
@@ -244,11 +362,11 @@ __END__
 
 =head1 SEE ALSO
 
-We live in splendid isolation.
+L<mu(1)>, L<mu-server(1)>
 
 =head1 AUTHOR
 
-  attila <attila@stalphonsos.com>
+attila <attila@stalphonsos.com>
 
 =head1 LICENSE
 
--- t/001-basics.t
+++ t/001-basics.t
@@ -2,14 +2,6 @@
 ##
 # 001-basics.t - basic mup tests
 ##
-use strict;
-use warnings;
-use Test::More tests => 1;
-
-sub run_test { 1; }
-
-ok(run_test(), 'pass test');
-
 # Copyright (C) 2015 by attila <attila@stalphonsos.com>
 # 
 # Permission to use, copy, modify, and distribute this software for any
@@ -24,6 +16,19 @@ ok(run_test(), 'pass test');
 # PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
 # TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 # PERFORMANCE OF THIS SOFTWARE.
+##
+use strict;
+use warnings;
+use Test::More tests => 3;
+use Data::Dumper;
+use mup;
+
+my $mu = mup->new(verbose => $ENV{'TEST_VERBOSE'});
+ok($mu,"constructor won: $mu");
+my $p = $mu->ping();
+ok($p,"ping returned:".Dumper($p));
+my $p2 = $mu->ping();
+ok($p2,"ping2 returned:".Dumper($p2));
 
 ##
 # Local variables:
